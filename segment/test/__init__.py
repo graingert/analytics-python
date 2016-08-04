6c623503a99b6889043962d150efcaf9dd04723c
Thomas Grainger <tom.grainger@procensus.com>
2016-08-04 17:32:17 +0100

    rename to segment

--- segment/test/__init__.py
+++ segment/test/__init__.py
@@ -3,9 +3,13 @@ import pkgutil
 import logging
 import sys
 
+from .. import test
+
+
 def all_names():
     for _, modname, _ in pkgutil.iter_modules(__path__):
-        yield 'analytics.test.' + modname
+        yield test.__name__ + '.' + modname
+
 
 def all():
     logging.basicConfig(stream=sys.stderr)
